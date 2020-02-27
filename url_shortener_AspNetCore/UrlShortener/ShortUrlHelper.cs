d940ea49f5ecd1b77e80e6c76ab52755fddf7029
Keith Pearce <keith@keith-pearce.co.uk>
2020-02-27 21:35:33 +0000

    Updating library to .netstandard so it can be used for both web apps

--- url_shortener_AspNetCore/UrlShortener/ShortUrlHelper.cs
+++ url_shortener_AspNetCore/UrlShortener/ShortUrlHelper.cs
@@ -4,12 +4,16 @@ using System.Text;
 
 namespace UrlShortenerLib
 {
+    /// <summary>
+    /// Bidirective conversion between natural numbers (int or IDs) and short strings
+    /// </summary>
     public class ShortUrlHelper
     {
 
         private const string Alphabet = "23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_";
         private static readonly int Base = Alphabet.Length;
 
+        // Encode() takes an int and turns it into a short string 
         public static string Encode(int num)
         {
             if (num == 0)
@@ -24,6 +28,7 @@ namespace UrlShortenerLib
             return string.Join(string.Empty, s.Reverse());
         }
 
+        // Decode() takes a short string and turns it into an int
         public static int Decode(string str)
         {
             var num = 0;

696275f34bda5c359758c33104c3bddc76275c79
Keith Pearce <keith@keith-pearce.co.uk>
2020-02-27 21:39:10 +0000

    Adding webforms application to solution

---
+++ url_shortener_AspNetCore/url_shortener_WebForms/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.UI;
+
+namespace url_shortener_WebForms
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit https://go.microsoft.com/fwlink/?LinkID=303951
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            Bundle bootstrap = new StyleBundle("~/bundle/Bootstrap")
+                                 .Include("~/Content/bootstrap-theme.min.css",
+                                          "~/Content/bootstrap.min.css");
+
+
+            bundles.Add(bootstrap);
+        }
+    }
+}
---
+++ url_shortener_AspNetCore/url_shortener_WebForms/App_Start/ScriptMapping.cs
@@ -0,0 +1,38 @@
+﻿using System.Web.UI;
+
+namespace url_shortener_WebForms.App_Start
+{
+    public static class ScriptMapping
+    {
+        public static void AddDefinitions()
+        {
+            ScriptManager.ScriptResourceMapping.AddDefinition(
+                "Jquery",
+                new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/jquery-3.4.1.min.js",
+                    DebugPath = "~/Scripts/jquery-3.4.1.js"
+                });
+            ScriptManager.ScriptResourceMapping.AddDefinition(
+                "Bootstrap",
+                new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/bootstrap.min.js",
+                    DebugPath = "~/Scripts/bootstrap.js"
+                });
+            ScriptManager.ScriptResourceMapping.AddDefinition(
+                "General",
+                new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/general.js",
+                });
+            ScriptManager.ScriptResourceMapping.AddDefinition(
+                "JqueryValid",
+                new ScriptResourceDefinition
+                {
+                    Path = "~/Scripts/jquery.validate.min.js",
+                    DebugPath = "~/Scripts/jquery.validate.js"
+                });
+        }
+    }
+}
